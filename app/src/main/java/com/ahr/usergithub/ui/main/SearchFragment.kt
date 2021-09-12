721a97051f233c2efe9d3e7fff1c77231dee2246
abdulhafizr <abdulhafizramadan.17@gmail.com>
2021-09-12 09:43:09 +0700

    | Upgrade "Search User" features progress 1 (search feature checkpoint)

--- app/src/main/java/com/ahr/usergithub/ui/main/SearchFragment.kt
+++ app/src/main/java/com/ahr/usergithub/ui/main/SearchFragment.kt
@@ -1,8 +1,15 @@
 package com.ahr.usergithub.ui.main
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.*
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import com.ahr.usergithub.MainActivity
 import com.ahr.usergithub.R
 import com.ahr.usergithub.databinding.FragmentSearchBinding
 
@@ -15,25 +22,54 @@ class SearchFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        setHasOptionsMenu(true)
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
+        (activity as MainActivity).apply {
+            setSupportActionBar(binding.toolbarSearch)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+        setHasOptionsMenu(true)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.tieSearch.apply {
+            addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 
-        binding.toolbarSearch.apply {
-//            inflateMenu(R.menu.search_menu)
-//            menu.performIdentifierAction(R.id.search_action, 0)
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    Log.d(TAG, "onTextChanged: $s")
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                    Log.d(TAG, "afterTextChanged: $s")
+                }
+            })
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        super.onCreateOptionsMenu(menu, inflater)
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            activity?.onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
+
+//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+//        super.onCreateOptionsMenu(menu, inflater)
+//        inflater.inflate(R.menu.search_menu, menu)
+//        menu.performIdentifierAction(R.id.search_action, 0)
+//    }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    companion object {
+        private const val TAG = "SearchFragment"
     }
 }
