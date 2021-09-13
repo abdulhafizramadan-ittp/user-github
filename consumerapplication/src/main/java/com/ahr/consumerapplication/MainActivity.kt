fd13bc21fa6f7cb11fd0dd2c65ccfb8a617cf724
abdulhafizr <abdulhafizramadan.17@gmail.com>
2021-09-13 18:45:39 +0700

    | Add feature show favorite user in consumer application

--- consumerapplication/src/main/java/com/ahr/consumerapplication/MainActivity.kt
+++ consumerapplication/src/main/java/com/ahr/consumerapplication/MainActivity.kt
@@ -5,15 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
-import android.view.View
-import androidx.lifecycle.ViewModelProvider
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ahr.consumerapplication.adapter.ListAdapter
-import com.ahr.consumerapplication.database.DatabaseContract
 import com.ahr.consumerapplication.database.DatabaseContract.UserColumns.Companion.CONTENT_URI
 import com.ahr.consumerapplication.databinding.ActivityMainBinding
 import com.ahr.consumerapplication.helper.MappingHelper
-import com.ahr.consumerapplication.viewmodel.ListViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,6 +32,7 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             rvUsers.adapter = adapter
             rvUsers.layoutManager = LinearLayoutManager(this@MainActivity)
+            rvUsers.setHasFixedSize(true)
         }
 
         val handlerThread = HandlerThread("DataObserver")
@@ -45,11 +47,21 @@ class MainActivity : AppCompatActivity() {
         }
 
         contentResolver.registerContentObserver(CONTENT_URI, true, myObserver)
+
+        loadUserFromContentProvider()
     }
 
     private fun loadUserFromContentProvider() {
-        val cursor = contentResolver.query(CONTENT_URI, null, null, null, null)
-        val notes = MappingHelper.mapCursorToArrayList(cursor)
-        adapter.setListUser(notes)
+        GlobalScope.launch(Dispatchers.Main) {
+            val differedUser = async(Dispatchers.IO) {
+                val cursor = contentResolver.query(CONTENT_URI, null, null, null, null)
+                MappingHelper.mapCursorToArrayList(cursor)
+            }
+
+            val users = differedUser.await()
+            Log.d("MainActivity", "loadUserFromContentProvider: notes = $users")
+            adapter.setListUser(users)
+        }
+
     }
 }
